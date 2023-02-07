a77c1268d9f3dfb34100ff701f9df20b2eee5021
abass-dev <abassthedev@gmail.com>
2023-02-07 21:04:35 +0100

    end

--- App.tsx
+++ App.tsx
@@ -1,22 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { Button, View } from 'react-native'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import * as Notifications from 'expo-notifications'
+import {useState, useEffect} from 'react'
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowAlert: true
+}),})
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener((notification) => {
+      console.log(notification)
+      const userName = notification.request.content.data.username
+      console.log(userName)
+    })
+    return () => {
+      subscription.remove()
+    }
+  }, [])
+async function scheduleNotificationHandler() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'My notification',
+      body: 'The body of my notificatio',
+      data: {username: 'Max'}
+    },
+      trigger: {seconds: 5}
+  })
+}
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+      <Button onPress={async () => {scheduleNotificationHandler()}} title='Schedule notification'/>
+      </View>
     );
   }
-}
+
