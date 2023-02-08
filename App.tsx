ede9c8a6c4f45bde518cb32c0cc2fcf04242e62f
abass-dev <abassthedev@gmail.com>
2023-02-08 23:25:23 +0100

    feat: add push notifications

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Button, View } from 'react-native'
+import { Button, View, Alert, Platform} from 'react-native'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -14,17 +14,44 @@ Notifications.setNotificationHandler({
 }),})
 
 export default function App() {
+  const [token, setToken] = useState()
   useEffect(() => {
+    async function configurePushNotification() {
+      const { status } = await Notifications.getPermissionsAsync()
+      const finalStatus = status
+      
+      if(finalStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync()
+        finalStatus = status
+      }
+      if(finalStatus !== 'granted') {
+        Alert.alert('Permission required:', 'Push notifications need the appropriate permissions')
+      return;
+      }
+     const pushTokenData = await Notifications.getExpoPushTokenAsync()
+     setToken(pushTokenData)
+     
+     if(Platform.OS === 'android') {
+       Notifications.setNotificationChannelAsync('default', {
+         name: 'default',
+         importance: Notifications.AndroidImportance.FEFAULT
+       })
+     }
+    }
+    configurePushNotification()
+  }, [])
+  useEffect(() => {
+     
     const subscription = Notifications.addNotificationReceivedListener((notification) => {
-      console.log(notification)
+      //console.log(notification)
       const userName = notification.request.content.data.username
-      console.log(userName)
+      //console.log(userName)
     })
     return () => {
       subscription.remove()
     }
   }, [])
-async function scheduleNotificationHandler() {
+    async function scheduleNotificationHandler() {
   await Notifications.scheduleNotificationAsync({
     content: {
       title: 'My notification',
@@ -34,9 +61,23 @@ async function scheduleNotificationHandler() {
       trigger: {seconds: 5}
   })
 }
+    function sendPushNotificationHandler() {
+  fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      to: token.data,
+      title: 'Push notifications title',
+      body: 'Push notifications body'
+    })
+})
+}
     return (
       <View style={{flex: 1, justifyContent: 'center'}}>
       <Button onPress={async () => {scheduleNotificationHandler()}} title='Schedule notification'/>
+      <Button onPress={() => {sendPushNotificationHandler()}} title='Push notification'/>
       </View>
     );
   }
